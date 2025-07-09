58aab0559d1b23ff4673fc43f2522df5dd908bd5
liamhuynh19 <hlty@cmcglobal.vn>
2025-07-09 08:55:00 +0700

    user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_07_031631) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_08_101939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_07_031631) do
     t.string "creator"
     t.text "description"
     t.date "published_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "role"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
