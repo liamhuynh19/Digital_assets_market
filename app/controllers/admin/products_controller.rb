26dbdfa6fa872f7ba1c6c04055ef855f5ac078ef
liamhuynh19 <hlty@cmcglobal.vn>
2025-07-16 09:32:27 +0700

    admin routes

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,49 @@
+class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: "Product was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: "Product was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: "Product was successfully deleted."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.expect(product: [ :name, :description, :price, :file_url, :category_id ])
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_many :products, dependent: :destroy
   validates_uniqueness_of(:name)
 end
