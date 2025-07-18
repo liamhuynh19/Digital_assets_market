278b2150b41a980a7d2d5252e7e1c71cea82a09a
liamhuynh19 <hlty@cmcglobal.vn>
2025-07-18 08:56:37 +0700

    rm unused test

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+# require "test_helper"
 
-class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class TagTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
