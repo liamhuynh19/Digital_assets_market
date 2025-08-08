d073b6d10d6e5fb6b7a0f191047c374ad3f6150c
liamhuynh19 <hlty@cmcglobal.vn>
2025-08-08 16:48:36 +0700

    correct redis config url

--- config/initializers/sidekid.rb
+++ config/initializers/sidekid.rb
@@ -1,13 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379/1" } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
-end
-
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.logger.level = Logger::WARN
-  end
+  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379/1" } }
 end
