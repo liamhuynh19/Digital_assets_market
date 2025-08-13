fc4303790d1f20d099d24a48606720b6350583cf
liamhuynh19 <hlty@cmcglobal.vn>
2025-08-13 10:42:08 +0700

    modify admin and seller dashboard

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -15,18 +15,28 @@ class Admin::ReportsController < ApplicationController
   private
   def build_admin_report
     @total_users = User.count
-    @total_products = Product.count
-    @total_orders = Order.count
+    @product = Product.all
+    @total_products = @product.count
+    @published_products = @product.where(status: "published").count
+
     @orders = Order.order(created_at: :desc)
-    @paid_orders = @orders.where(status: "paid")
     @total_orders = @orders.count
-    @total_reviews = Review.count
+    @paid_orders = @orders.where(status: "paid")
+    @paid_orders_count = @paid_orders.count
+    @total_orders = @orders.count
     @revenue = @paid_orders.sum(:total_amount)
-    @top_products = Product.joins(:order_items)
-                           .group("products.id")
-                           .order("COUNT(order_items) DESC")
-                           .limit(3)
-    @top_sellers = User.joins(products: :order_items)
+    @total_reviews = Review.count
+
+    @top_products = Product
+      .joins(order_items: :order)
+      .where(orders: { status: "paid" })
+      .select("products.*, COUNT(order_items) AS units_sold")
+      .group("products.id")
+      .order("units_sold DESC")
+      .includes(:category, :thumbnail_attachment)
+    # .limit(3)
+    @top_sellers = User.joins(products: [ order_items: :order ])
+                      .where(orders: { status: "paid" })
                        .select("users.*, COUNT(order_items.id) AS items_sold")
                        .group("users.id")
                        .order("items_sold DESC")
@@ -37,17 +47,20 @@ class Admin::ReportsController < ApplicationController
     @products = current_user.products
     @product_ids = @products.select(:id)
     @paid_items = OrderItem.joins(:order).where(product_id: @product_ids, orders: { status: "paid" })
-
+    @paid_orders_count = @paid_items.distinct.count("orders.id")
 
     @total_products = @products.count
+    @published_products = @products.where(status: "published").count
     @total_reviews = Review.where(product_id: @product_ids).count
     @average_rating = Review.where(product_id: @product_ids).average(:rating) || 0
-    @total_orders = OrderItem.joins(:order).where(product_id: @product_ids, orders: { status: "paid" }).distinct.count("orders.id")
+    @total_orders = OrderItem.joins(:order).where(product_id: @product_ids).distinct.count("orders.id")
     @top_products = @products
-                     .joins(:order_items)
+                     .joins(order_items: :order)
+                     .where(orders: { status: "paid" })
+                     .includes(:category, :thumbnail_attachment)
                      .select("products.*, COUNT(order_items.id) AS items_sold")
                      .group("products.id")
                      .order("items_sold DESC")
-    @total_revenue = @paid_items.sum("order_items.price_at_purchase")
+    @revenue = @paid_items.sum("order_items.price_at_purchase")
   end
 end
