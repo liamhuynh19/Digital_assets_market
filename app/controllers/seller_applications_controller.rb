7f2c52975c13c511dc06d4804d94c81b418add79
liamhuynh19 <hlty@cmcglobal.vn>
2025-08-20 11:11:04 +0700

    enhance seller application

--- app/controllers/seller_applications_controller.rb
+++ app/controllers/seller_applications_controller.rb
@@ -15,7 +15,7 @@ class SellerApplicationsController < ApplicationController
     return redirect_to root_path, alert: "You are already a seller or have a pending application." unless current_user.can_apply_for_seller?
     @seller_application = current_user.seller_applications.build(seller_application_params)
     if @seller_application.save
-      redirect_to root_path, notice: "Your seller application has been submitted successfully. We will review it shortly."
+      redirect_to seller_application_path, notice: "Your seller application has been submitted successfully. We will review it shortly."
     else
       render :new, status: :unprocessable_entity
     end
