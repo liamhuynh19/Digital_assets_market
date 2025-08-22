11307178dd4450c62c8d839c30b650d37b80256a
liamhuynh19 <hlty@cmcglobal.vn>
2025-08-22 09:19:26 +0700

    fix user name not include

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # , :rack_mini_profiler_authorize_request?
   include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?  # Add this line
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -60,6 +61,7 @@ class ApplicationController < ActionController::Base
   # end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :email, :password, :password_confirmation, :phone_number ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :email, :password, :password_confirmation, :phone_number ])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -167,7 +167,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_171713) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
-    t.string "role"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -177,6 +176,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_171713) do
     t.datetime "remember_created_at"
     t.string "encrypted_phone_number"
     t.string "encrypted_phone_number_iv"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["encrypted_phone_number_iv"], name: "index_users_on_encrypted_phone_number_iv", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
